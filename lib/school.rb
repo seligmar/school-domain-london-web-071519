97fc0965e144bdfdf9a7d8052ec85f6299b5ac40
Mary Selig <seligmar@gmail.com>
2019-07-17 17:45:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,7 +2,7 @@ require "pry"
 
 class School 
   # attr_accessor :grade 
-   attr_reader :name, :roster, :grade  
+ #  attr_reader :name, :roster, :grade  
   
   def initialize(name)
     @name = name, 
