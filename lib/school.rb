a1e3c1c6960ce538d3d28fe4969d8415329ad2e8
Mary Selig <seligmar@gmail.com>
2019-07-17 17:36:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require "pry" 
+
+class School 
+  
+ # attr_accessor :grade 
+  attr_reader :name, :roster,  
+  
+  def initialize(name)
+    @name = name, @roster = {}
+  end 
+
+  def add_student(name, grade)
+    @name = name 
+    @grade = grade 
+    if @roster[@grade] 
+       @roster[@grade] << @name
+     else 
+       @roster[@grade] = [@name]
+   end 
+  end 
+
+end 
+
+
+ # binding.pry
+#  @roster = roster
+ # @grade = grade 
+
